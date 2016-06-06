ac1adcab1c3d495da589214c3596b3eceedd0d1c
David Ferguson <deejai@live.com>
2016-06-06 18:28:46 -0400

    Now creates a window using GUI.Window

--- Pong.py
+++ Pong.py
@@ -1,9 +1,16 @@
+import sys
+
+from PyQt4 import QtGui
+
 import Core.GameLoop
+import GUI.Window
 
 # Initialize things, run the game loop, clean up on exit
 
 def main():
-	Core.GameLoop.loopy()
+	app = QtGui.QApplication(sys.argv)
+	mainWindow = GUI.Window.Window()
+	sys.exit(app.exec_())
 
 if __name__ == "__main__":
 	main()
