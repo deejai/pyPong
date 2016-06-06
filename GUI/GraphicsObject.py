b83b39d6598bc55daf8100867a5d04df02c437a9
David Ferguson <deejai@live.com>
2016-06-06 18:38:16 -0400

    Initial class created

--- GUI/GraphicsObject.py
+++ GUI/GraphicsObject.py
@@ -1,7 +1,10 @@
-class GraphicsObject:
+from PyQt4 import QtGui
+
+class GraphicsObject():
     
-    def __init__():
-        self.shape = 
+    def __init__(self):
+        self.shape = rectangle
+        self.dimensions = (0, 0, 0)
         
     def render():
-        
+        pass
