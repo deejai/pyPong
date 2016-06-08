88f364d1b0fe12177823bbc252cfb9c21b56b281
David Ferguson <deejai@live.com>
2016-06-08 18:22:05 -0400

    class name corrected to match module name

--- Display/HighScores.py
+++ Display/HighScores.py
@@ -1,6 +1,6 @@
 from PyQt4 import QtGui
 
-class HighScore(QtGui.QGraphicsView):
+class HighScores(QtGui.QGraphicsView):
     'Table for storing and displaying high scores'
     
     def __init__(self, filePath, isRelative):
