2471ffa95062101c45b7b7fad9521fdb64b3adac
David Ferguson <deejai@live.com>
2016-06-08 17:19:27 -0400

    Player now holds Paddle instead of the other way around

--- Core/Paddle.py
+++ Core/Paddle.py
@@ -3,16 +3,13 @@ from PyQt4 import QtGui
 class Paddle(QtGui.QGraphicsRectItem):
     'A pong paddle'
 
-    def __init__(self, length, color):
+    def __init__(self, length, color, speed):
         super(Paddle, self).__init__()
 
-        this.speed = 1
+        this.speed = speed
         this.length = length
         this.width = length/6
         this.color = color
 
     def setSpeed(speed):
         this.speed = speed
-
-    def setController(controller):
-        self.controller = controller
