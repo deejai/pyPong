1f39e50323536f938fe0cc80b72458d983aacaab
deejai <deejai@live.com>
2016-06-08 00:21:48 -0400

    Added getMove function

--- Controller/AI.py
+++ Controller/AI.py
@@ -1,6 +1,11 @@
 class AI:
     'A virtual pong player'
 
-    def __init__(self, name, paddle):
+    def __init__(self, name):
         this.name = name
         this.paddle = paddle
+
+    def getMove(opponentPos, opponentVel, BallPos, BallVel):
+        return 1 #up
+        # return -1 #down
+        # return 0 #stay
--- Controller/Player.py
+++ Controller/Player.py
@@ -1,8 +1,12 @@
 class Player:
     'A human pong player'
 
-    def __init__(self, name, paddle):
+    def __init__(self, name):
         this.name = name
         this.paddle = paddle
 
+    def getMove(opponentPos, opponentVel, BallPos, BallVel):
+        return 1 #up
+        # return -1 #down
+        # return 0 #stay
     
