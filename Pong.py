169a0659cd74e599a674bfe7bb498b819d1e0cc1
David Ferguson <deejai@live.com>
2016-06-08 18:03:30 -0400

    main now successfully initializes an arena, but graphics are not yet displayed

--- Pong.py
+++ Pong.py
@@ -2,15 +2,36 @@ import sys
 
 from PyQt4 import QtGui
 
-from Core.GameLoop import GameLoop
-from Display.Window import Window
+from Core.GameLoop    import GameLoop
+from Core.Paddle      import Paddle
+from Display.Arena    import Arena
+from Display.Window   import Window
+from Display.MainMenu import MainMenu
+from Control.Player   import Player
+from Control.AI       import AI
+from Control.Human    import Human
 
 # Initialize things, run the game loop, clean up on exit
 
+white = QtGui.QColor(255, 255, 255)
+
 def main():
-	app = QtGui.QApplication(sys.argv)
-	mainWindow = Window()
-	sys.exit(app.exec_())
+    app = QtGui.QApplication(sys.argv)
+    window = Window()
+    # mainMenu = MainMenu()
+    # window.setView(mainMenu)
+    
+    paddle1 = Paddle(10, white, 1)
+    paddle2 = Paddle(10, white, 1)
+    
+    player1 = Human("Charlie", paddle1, white)
+    player2 = AI("Doug", paddle2, white)
+        
+    testArena = Arena()
+    testArena.setPlayers(player1, player2)
+    window.setView(testArena)
+    
+    sys.exit(app.exec_())
 
 if __name__ == "__main__":
-	main()
+    main()
