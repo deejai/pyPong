56b4cfb4e25346df80f5994f06aa22a6a85b9a32
David Ferguson <deejai@live.com>
2016-06-08 18:05:37 -0400

    now initializes with name, paddle, and color

--- Control/AI.py
+++ Control/AI.py
@@ -3,8 +3,8 @@ from Control.Player import Player
 class AI(Player):
     'A virtual pong player'
 
-    def __init__(self):
-        super(AI, self).__init__()
+    def __init__(self, name, paddle, color):
+        super(AI, self).__init__(name, paddle, color)
 
     def getMove(self, oppPos, opptVel, ballPos, ballVel):
         # TODO: implement AI
--- Control/Human.py
+++ Control/Human.py
@@ -3,14 +3,14 @@ from Control.Player import Player
 class Human(Player):
     'A human pong player'
 
-    def __init__(self):
-        super(AI, self).__init__()
+    def __init__(self, name, paddle, color):
+        super(Human, self).__init__()
 
     def getMove(self, upPressed, downPressed):
         if( upPressed ):
-            return 1 if !downPressed else 0
-        else if( downPressed ):
-            return -1 if !upPressed else 0
+            return 1 if not downPressed else 0
+        elif( downPressed ):
+            return -1 if not upPressed else 0
         else:
             return 0
             
