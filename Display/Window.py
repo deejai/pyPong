a89d3204a05d83ee3a494649c3346b9ab44485bc
deejai <deejai@live.com>
2016-06-09 15:17:19 -0400

    Window is now a QGraphicsView instead of a QMainWindow

--- Display/Window.py
+++ Display/Window.py
@@ -1,19 +1,20 @@
 from Display import MainMenu
 
-from PyQt4 import QtGui
+from PyQt4 import QtGui, QtCore
 
-class Window(QtGui.QMainWindow):
+class Window(QtGui.QGraphicsView, QtCore.QObject):
     
     def __init__(self):
         super(Window, self).__init__()
-        self.setGeometry(50, 50, 480, 360)
+        self.setFixedSize(480, 360)
+        self.setAlignment(QtCore.Qt.AlignTop|QtCore.Qt.AlignLeft)
+        self.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+        self.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
         self.setWindowTitle("Davepong")
+
         # TODO: self.setWindowIcon(ICON_FILE_PATH)
         
         # self.scene = Display.MainMenu()
 
     def setView(self, qview):
         self.show()
-    
-if __name__ == "__main__":
-    main()
